41e3237ea6cca4e3c6b0c58aef7e23e0ff5655bc
Christoph Göttert <goettert@pathmate-technologies.com>
2018-11-12 09:02:35 +0100

    new classname.

--- src/Message.js
+++ src/Message.js
@@ -64,7 +64,7 @@ export default class Message extends React.Component {
   render() {
     const sameUser = isSameUser(this.props.currentMessage, this.props.nextMessage);
     return (
-      <View>
+      <View className='message-container'>
         {this.renderDay()}
         {this.props.currentMessage.system ? (
           this.renderSystemMessage()
